1c215706181cfd86f826fa7f3c193558148cda89
Vitor Guimarães <vitorguimaraes.ce@gmail.com>
2022-02-24 07:03:49 -0300

    handle files with case

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,3 +1,8 @@
 defmodule ReportsGenerator do
-
+  def build(filename) do
+    # Tratamento com case
+    case file = File.read("reports/#{filename}") do
+      {:ok, result} -> result
+      {:error, reason} -> reason
+    end
 end

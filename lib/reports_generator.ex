0229254d98e51f5a5cd39869851a7a24f8a608c6
Vitor Guimarães <vitorguimaraes.ce@gmail.com>
2022-03-26 08:42:49 -0300

    handle files with pipe operator, file stream and enum.map. Map applies a map operation in line with parse_line() function

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,7 +1,16 @@
 defmodule ReportsGenerator do
   def build(filename) do
     "reports/#{filename}"
+    # Reads file line by line
     |> File.stream!()
-    |> Enum.each(fn elem -> IO.inspect(elem) end)
+    # Applies a map operation in line with parse_line() function
+    |> Enum.map(fn line -> parse_line(line) end)
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(2, &String.to_integer/1)
   end
 end

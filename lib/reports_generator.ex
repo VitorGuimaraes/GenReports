21b7d444a22c43d1a51f376027e0cc7d21cc078e
Vitor Guimarães <vitorguimaraes.ce@gmail.com>
2022-03-26 09:49:58 -0300

    sum order prices of each id

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -4,9 +4,9 @@ defmodule ReportsGenerator do
     # Reads file line by line
     |> File.stream!()
     # Creates a map with the data: %{id, food_name, price}
-    |> Enum.reduce(%{}, fn line, report_map -> # report_map is the accumulator
+    |> Enum.reduce(report_acc(), fn line, report_map -> # report_map is the accumulator
       [id, _food_name, price] = parse_line(line) # using pattern matching
-      Map.put(report_map, id, price)
+      Map.put(report_map, id, report_map[id] + price) # sum order prices of each id
     end)
   end
 
@@ -16,4 +16,6 @@ defmodule ReportsGenerator do
     |> String.split(",")
     |> List.update_at(2, &String.to_integer/1)
   end
+
+  defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 end
